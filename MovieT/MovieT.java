c8259f898cbbf5bad17fe6b5fd0cb6c5f1ebd22a
박은비 <eunbi628268@gmail.com>
2022-04-24 22:23:46 +0900

    [update] MovieT base

---
+++ MovieT/MovieT.java
@@ -0,0 +1,18 @@
+package MovieT;
+
+class Movie{
+	String movieName;
+	
+}
+
+
+
+
+public class MovieT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
